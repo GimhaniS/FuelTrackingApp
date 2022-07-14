b1ecad2f03d2b4c1c886ae108e51d90b39d726e3
gimhani_ks <gimhanis1996@gmail.com>
2022-07-14 11:19:28 +0530

    dependencies added.

---
+++ client/fuelTrackingApp/app/components/SearchCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const SearchCard = () => {
+  return (
+    <View>
+      <Text>SearchCard</Text>
+    </View>
+  );
+};
+
+export { SearchCard };
+
+const styles = StyleSheet.create({});
--- client/fuelTrackingApp/app/screens/homeScreen/HomeScreen.js
+++ client/fuelTrackingApp/app/screens/homeScreen/HomeScreen.js
@@ -1,9 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import MapView from 'react-native-maps';
+import GeoLocation from '@react-native-community/geolocation';
+import { request, PERMISSIONS } from 'react-native-permissions';
+import { COLORS } from '../../utils/Colors';
 const HomeScreen = () => {
+  const [currentLatitude, setCurrentLatitude] = useState();
+  const [currentLongitude, setCurrentLongitude] = useState();
+  const requestLocationPermission = async () => {
+    if (Platform.OS === 'android') {
+      const res = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      console.log('res===>', res);
+      if (res === 'granted') {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+
+  const getCurrentLocation = () => {
+    GeoLocation.getCurrentPosition(
+      (position) => {
+        const currentLat = position.coords.latitude;
+        const currentLong = position.coords.longitude;
+        console.log('latitude====>', currentLat);
+        console.log('longitude====>', currentLong);
+        setCurrentLatitude(currentLat);
+        setCurrentLongitude(currentLong);
+        return true;
+      },
+      (error) => {
+        console.log('ERROR==>', error);
+        return false;
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+      }
+    );
+  };
+
+  useEffect(() => {
+    requestLocationPermission();
+    getCurrentLocation();
+  }, []);
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>HomeScreen</Text>
     </View>
   );
@@ -11,4 +56,10 @@ const HomeScreen = () => {
 
 export { HomeScreen };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: COLORS.background,
+    flex: 1,
+  },
+});
